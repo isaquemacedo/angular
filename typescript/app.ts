cb125e13911d2591c1c6f16d9f5e2cffffbb0587
Isaque Alves <isaque.mace@gmail.com>
2018-04-27 01:44:17 -0300

    Primeiros passos - type script e configuração do Atom

---
+++ typescript/app.ts
@@ -0,0 +1,11 @@
+let message: string = "Help me, Obi-Wan Kenobi. You're my only hope!"
+console.log(message);
+
+let episode: number = 4
+console.log(episode)
+episode++
+console.log("Next episode is " + episode)
+
+let favoriteDroid: string
+favoriteDroid = "BB-8"
+console.log("My favorite droind is " + favoriteDroid)
---
+++ typescript/dist/app.js
@@ -0,0 +1,9 @@
+var message = "Help me, Obi-Wan Kenobi. You're my only hope!";
+console.log(message);
+var episode = 4;
+console.log(episode);
+episode++;
+console.log("Next episode is " + episode);
+var favoriteDroid;
+favoriteDroid = "BB-8";
+console.log("My favorite droind is " + favoriteDroid);
