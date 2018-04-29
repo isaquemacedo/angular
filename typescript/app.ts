1ce6285d75498b2f36e3783b8b37895ae70e9f9e
Isaque Alves <isaque.mace@gmail.com>
2018-04-29 14:59:39 -0300

    funções em type script: template string, arrow function e funções padroes

--- typescript/app.ts
+++ typescript/app.ts
@@ -9,3 +9,24 @@ console.log("Next episode is " + episode)
 let favoriteDroid: string
 favoriteDroid = "BB-8"
 console.log("My favorite droind is " + favoriteDroid)
+
+let isEnoughToBeatMF = function (parsecs: number): boolean {
+    return parsecs < 12;
+}
+
+let distance = 11
+console.log(`Is ${distance} parsecs enough to beat Millenium Falcon ? ${isEnoughToBeatMF(distance) ? 'YES': 'No'}`);
+
+let call = (name: string) => console.log(`Do you copy, ${name}? `);
+call('R2')
+
+function inc(speed: number, inc: number = 1): number {
+    return speed + inc
+}
+
+let whatsIsInc = (inc: number)  => inc <= 0 ? 0: inc;
+
+console.log(`ìnc (5,1) = ${inc(5,1)}`)
+console.log(`inc (5) = ${inc(5)}`)
+console.log(`inc is 0 = ${inc(whatsIsInc(0))}`)
+console.log(`inc is 8 = ${inc(whatsIsInc(8))}`)
--- typescript/dist/app.js
+++ typescript/dist/app.js
@@ -7,3 +7,19 @@ console.log("Next episode is " + episode);
 var favoriteDroid;
 favoriteDroid = "BB-8";
 console.log("My favorite droind is " + favoriteDroid);
+var isEnoughToBeatMF = function (parsecs) {
+    return parsecs < 12;
+};
+var distance = 11;
+console.log("Is " + distance + " parsecs enough to beat Millenium Falcon ? " + (isEnoughToBeatMF(distance) ? 'YES' : 'No'));
+var call = function (name) { return console.log("Do you copy, " + name + "? "); };
+call('R2');
+function inc(speed, inc) {
+    if (inc === void 0) { inc = 1; }
+    return speed + inc;
+}
+var whatsIsInc = function (inc) { return inc <= 0 ? 0 : inc; };
+console.log("\u00ECnc (5,1) = " + inc(5, 1));
+console.log("inc (5) = " + inc(5));
+console.log("inc is 0 = " + inc(whatsIsInc(0)));
+console.log("inc is 8 = " + inc(whatsIsInc(8)));
